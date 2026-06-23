fb82925f4142cf7b884c3aad6efddcdd39758b3a
bayeshevan-gif <bayeshev.a.n@gmail.com>
2026-06-23 09:13:31 +0500

    Delete src directory

--- src/lib/prisma.ts
+++
@@ -1,7 +0,0 @@
-// Временная заглушка вместо реальной Prisma для успешного деплоя фротенда
-export const prisma = new Proxy({} as any, {
-  get: () => {
-    // Возвращает пустую функцию на любой вызов вроде prisma.user.findMany()
-    return () => Promise.resolve([]);
-  }
-});
